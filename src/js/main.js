f442c2518bc2bcaaf2f3ca1053a6828b2695a397
Manuela Garcia Yebra <manuelagarciayebra@gmail.com>
2022-07-15 18:00:18 +0200

    fetch in api

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,28 @@
 'use strict';
 
 console.log('>> Ready :)');
+
+const ul = document.querySelector('.js-ul');
+let animeData = [];
+
+const renderAnimeData = () => {
+  let html = '';
+  for (const anime of animeData) {
+    html += `<li><article><img src='${anime.images.jpg.image_url}'/>`;
+    html += `<h2>${anime.title}</h2>`;
+    html += `</article></li>`;
+    console.log(anime.images.jpg.image_url);
+  }
+  ul.innerHTML = html;
+};
+
+const getApi = () => {
+  fetch('https://api.jikan.moe/v4/anime?q=naruto')
+    .then((response) => response.json())
+    .then((dataAnime) => {
+      animeData = dataAnime.data;
+      renderAnimeData();
+    });
+};
+
+getApi();
